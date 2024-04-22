9ef0027ec88be333c36a381fb4dd5f64b4b06352
Ronny <ronnysabo@gmail.com>
2024-04-22 14:40:42 +0200

    add textboxes with ai/user

--- src/app/components/chat.tsx
+++ src/app/components/chat.tsx
@@ -7,26 +7,51 @@ export default function Chat() {
 
   return (
     <div>
-      <ul>
-        {messages.map((m, index) => (
-          <li key={index}>
-            {m.role === "user" ? "User: " : "AI: "}
-            {m.content}
-          </li>
-        ))}
-      </ul>
-
-      <form onSubmit={handleSubmit}>
-        <label>
-          Say something...
+      <div className="flex justify-center mt-4">
+        <form
+          onSubmit={handleSubmit}
+          className="flex justify-center align-center w-3/4"
+        >
           <input
             value={input}
+            placeholder="Enter your message"
             onChange={handleInputChange}
-            className="text-black"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
-        </label>
-        <button type="submit">Send</button>
-      </form>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            Send
+          </button>
+        </form>
+      </div>
+      <div className="flex justify-center">
+        <ul className="w-3/4 my-10">
+          {messages.map((m, index) => (
+            <div
+              key={index}
+              className={`flex items-start my-1 ${
+                m.role === "user" ? "justify-start" : "justify-end"
+              }`}
+            >
+              <span>
+                {m.role === "user" ? "" : "🤖"}
+                {/* Replace these emojis with your preferred icons */}
+              </span>
+              <li
+                className={`rounded-lg p-2 ${
+                  m.role === "user"
+                    ? "order-1 bg-blue-600 mr-2 w-fit max-w-96"
+                    : "order-2 bg-gray-600 ml-2 w-fit max-w-96"
+                }`}
+              >
+                {m.content}
+              </li>
+            </div>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
