c4b83ef15aac2739c44710368273efa6cc6ee7be
Ronny <ronnysabo@gmail.com>
2024-04-22 16:10:08 +0200

    add css changes

--- src/app/components/chat.tsx
+++ src/app/components/chat.tsx
@@ -1,41 +1,55 @@
 "use client";
 
 import { useChat } from "ai/react";
+import { useEffect, useRef } from "react";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const chatParent = useRef<HTMLUListElement>(null);
 
+  useEffect(() => {
+    const domNode = chatParent.current;
+    if (domNode) {
+      domNode.scrollTop = domNode.scrollHeight;
+    }
+  });
   return (
-    <div>
-      <div className="flex justify-center mt-4">
+    <main className="flex flex-col w-full h-screen max-h-dvh bg-g">
+      <header>
+        <h1 className="w-1/2 flex justify-center mt-4 m-auto">AI Chattbot</h1>
+      </header>
+      <section className="flex justify-center mt-4">
         <form
           onSubmit={handleSubmit}
-          className="flex justify-center align-center w-3/4"
+          className="flex justify-center align-center w-2/4 gap-2"
         >
           <input
             value={input}
-            placeholder="Enter your message"
+            placeholder="Enter your question here..."
             onChange={handleInputChange}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
             type="submit"
           >
             Send
           </button>
         </form>
-      </div>
-      <div className="flex justify-center">
-        <ul className="w-3/4 my-10">
+      </section>
+      <div className="flex justify-center ">
+        <ul
+          ref={chatParent}
+          className="w-2/4 my-10 bg-black-900 max-h-[700px] overflow-y-auto rounded-lg scrollbar scrollbar-thumb-gray-700 scrollbar-track-gray-100 "
+        >
           {messages.map((m, index) => (
             <div
               key={index}
-              className={`flex items-start my-1 ${
-                m.role === "user" ? "justify-start" : "justify-end"
+              className={`flex items-start my-1 p-3 ${
+                m.role === "user" ? "justify-end" : "justify-start"
               }`}
             >
-              <span>
+              <span className="font-xxl text-3xl">
                 {m.role === "user" ? "" : "🤖"}
                 {/* Replace these emojis with your preferred icons */}
               </span>
@@ -52,6 +66,6 @@ export default function Chat() {
           ))}
         </ul>
       </div>
-    </div>
+    </main>
   );
 }
